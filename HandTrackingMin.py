6896a46d2f236dc2b5ee2a1e9fdeb5469c0aa85f
faezeh <faezehhesam.fh62@gmail.com>
2024-07-06 15:59:05 +0330

    "editerrocv2"

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -3,11 +3,38 @@ import mediapipe as mp
 import time
 
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils 
+
+
+pTime = 0
+eTmie = 0
 
 
 while True:
     success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
+    
+    
+    if result.multi_hand_landmarks:
+        for handLms in result.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+            
+    
+    
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    
+    cv2.putText(img,str(int(fps)),(10,70), cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+    
+    
+    
     
     
     
@@ -15,3 +42,5 @@ while True:
     cv2.waitKey(1)
 
 
+              
+              
